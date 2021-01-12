0f49dadd8469edad374dc48b1abf9d92332d39a9
hardikchavda <hardikkchavda@gmail.com>
2021-01-12 08:54:11 +0530

    Registeration Changes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,10 +5,8 @@ class Home extends CI_Controller
 {
 
 	public function index()
-	{
-		$this->load->view('header');
-		$this->load->view('home');
-		$this->load->view('footer');
+	{		
+		$this->load->view('home');	
 	}
 	
 }
--- application/views/registeruser.php
+++ application/views/registeruser.php
@@ -22,7 +22,7 @@
 						</div>
 						<div class="form-group">
 							<label for="uploadFile">Password</label>
-							<input name="uploadFile" type="text" class="form-control" id="uploadFile" placeholder="Upoload">
+							<input name="uploadFile" type="file" class="form-control" id="uploadFile" placeholder="Upoload">
 						</div>
 						<ul class="actions special">
 							<button type="submit" class="button style1 large">Submit</button>
@@ -36,20 +36,20 @@
 
 
 <script>
-	$(document).ready(function() {
-		$('#form-id').submit(function(evt) {
-			evt.preventDefault();
-			var url = $(this).attr('action');
-			var data = $(this).serialize();
-			$.post(url, data, function(o) {
-				if (o.result == 1) {
-					window.location.href = '<?= site_url('login'); ?>'
-				} else {
-					alert('invalid login');
-				}
-			}, 'json');
+	// $(document).ready(function() {
+	// 	$('#form-id').submit(function(evt) {
+	// 		evt.preventDefault();
+	// 		var url = $(this).attr('action');
+	// 		var data = $(this).serialize();
+	// 		$.post(url, data, function(o) {
+	// 			if (o.result == 1) {
+	// 				window.location.href = '<?= site_url('login'); ?>'
+	// 			} else {
+	// 				alert('invalid login');
+	// 			}
+	// 		}, 'json');
 
-		});
-	});
+	// 	});
+	// });
 </script>
 <?php $this->load->view('footer'); ?>
